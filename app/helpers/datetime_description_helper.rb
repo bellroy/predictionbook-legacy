ae54491217935ea1c9af4bc35aea2a8e5228dee8
Ivan Kozik <ivan@ludios.org>
2011-09-09 12:51:13 +0000

    For durations of over a month, show the date in YYYY-MM-DD format instead of a vague "N months" or "N years" string.

--- app/helpers/datetime_description_helper.rb
+++ app/helpers/datetime_description_helper.rb
@@ -1,12 +1,15 @@
 module DatetimeDescriptionHelper
   def time_in_words_with_context(time)
     time_str = time_ago_in_words(time)
-    if time <= Time.now
+    if time_str.include? "month" or time_str.include? "year"
+      # If it's been over a month, return the full date
+      "on #{time.strftime("%Y-%m-%d")}"
+    elsif time <= Time.now
       "#{time_str} ago"
     else
       "in #{time_str}"
     end
   rescue RangeError,NoMethodError # known issue in Rails
-    "in ages"
+    "on #{time.strftime("%Y-%m-%d")}"
   end
 end
