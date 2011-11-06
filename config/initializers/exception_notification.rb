6b503cf96ca3dede7f45a1e84a174ae7e3b4a77b
vijendra <vijendrakarkala@gmail.com>
2011-11-06 17:55:55 +0530

    removed unwanted files

--- config/initializers/exception_notification.rb
+++
@@ -1,7 +0,0 @@
-address = "errors@example.com"
-ExceptionNotifier.exception_recipients = address
-ExceptionNotifier.sender_address = %("pbook" <#{address}>)
-ExceptionNotifier.email_prefix = "[PBOOK-#{RAILS_ENV}-#{`hostname`.strip}] "
-
-require 'application'
-ApplicationController.send(:include, ExceptionNotifiable)
--- lib/array_ext.rb
+++
@@ -1,7 +0,0 @@
-class Array
-  def rsort(field)
-    sort {|a,b| b.send(field) <=> a.send(field)}
-  end
-  
-  alias_method :limit, :first
-end
