94c23ddc3268fbf8ca2145c6e32a49c85a0aae33
Glenn Francis Murray <glenn@metonymous.com>
2011-12-06 14:06:44 +1100

    Fixed custom matcher
    * expected array "&" behaviour, deprecated in Rails3 relations

--- spec/support/custom_matchers/in_order.rb
+++ spec/support/custom_matchers/in_order.rb
@@ -3,7 +3,7 @@ module CustomMatchers
     # depends on Array#& using the order of the first list
     # safe according to rubyspec.org
     match do |actual|
-      (actual & expected) == expected
+      (actual.to_a & expected.to_a) == expected
     end
   end
 end
